7b294731f07f4e4503e737ce409026ee7dab5bf3
Stanley Louis <stanlouis@msn.com>
2019-02-06 04:20:17 -0500

    Use temp recipes data for state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component, Fragment } from "react";
 import "./App.css";
-import { recipes } from "./data/tempDetails";
+import { recipes } from "./data/tempList";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
 class App extends Component {
+  state = {
+    recipes: recipes,
+  };
+
+  // getRecipes = async () => {
+  //   const url = `https://www.food2fork.com/api/search?key=${
+  //     process.env.REACT_APP_F2F_KEY
+  //   }`;
+  //   try {
+  //
+  //     const data = await fetch(url);
+  //     const jsonData = await data.json();
+  //
+  //     this.setState({
+  //       recipes: jsonData.recipes
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // componentDidMount() {
+  //   // this.getRecipes();
+  // }
+
+
   render() {
     return (
       <Fragment>
-        <RecipeList />
+        <RecipeList recipes={this.state.recipes}/>
         <RecipeDetails />
       </Fragment>
     );
