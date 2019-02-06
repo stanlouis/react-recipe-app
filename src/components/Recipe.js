8f1379534368a2ee82eba088d7bb0ef7c318c2f0
Stanley Louis <stanlouis@msn.com>
2019-02-06 05:26:42 -0500

    Add  Recipe and RecipeList content

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,54 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 
 class Recipe extends Component {
-    render() {
-        return (
-            <Fragment>
-                <h1>Hello from Recipe</h1>
-            </Fragment>
-        );
-    }
+  render() {
+    const {
+      image_url,
+      title,
+      source_url,
+      publisher,
+      recipe_id,
+    } = this.props.recipe;
+    return (
+      <Fragment>
+        <div className="col-10 mx-auto col-md6 col-lg-4 my-3">
+          <div className="card">
+            <div className="view overlay">
+              <a href="!#">
+                <img
+                  src={image_url}
+                  alt="recipe-pic"
+                  style={{ height: 'auto', width: '100%' }}
+                  className="img-card-top"
+                />
+                <div className="mask rgba-white-slight" />
+              </a>
+            </div>
+
+            <div className="card-body text-capitalize">
+              <h6>{title}</h6>
+              <h6 className="text-warning text-slanted">
+                provided by {publisher}
+              </h6>
+            </div>
+            <div className="card-footer">
+              <button className="btn btn-light-green text-capitalize">
+                details
+              </button>
+              <a
+                href={source_url}
+                className="btn btn-dark-green mx-2 text-capitalize"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Recipe <i class="fas fa-external-link-square-alt" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default Recipe;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,17 +1,30 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from "react";
 import Recipe from "./Recipe";
 import RecipeSearch from "./RecipeSearch";
 
 class RecipeList extends Component {
-    render() {
-        return (
-            <Fragment>
-                <h1>Hello from RecipeList</h1>
-                <RecipeSearch/>
-                <Recipe/>
-            </Fragment>
-        );
-    }
+  render() {
+    const { recipes } = this.props;
+    return (
+      <Fragment>
+        <RecipeSearch />
+        <div className="container my-5">
+          {/*title*/}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/*end*/}
+          <div className="row">
+            {recipes.map(recipe => {
+              return <Recipe key={recipe.recipe_id} recipe={recipe} />;
+            })}
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default RecipeList;
